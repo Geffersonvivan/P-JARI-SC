37c563e30d7efbc66aaf7b7d6b3269c0c90bb182
Gefferson Vivan <geffersonvivan@MacBook-Air-de-Gefferson.local>
2026-02-26 22:27:52 -0300

    Corrige render 500 do allauth auto-criando o SocialApp

--- start.sh
+++ start.sh
@@ -7,8 +7,11 @@ python manage.py migrate --noinput
 echo "Ensuring Site object exists..."
 python manage.py shell -c "
 from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
 from django.conf import settings
-Site.objects.get_or_create(id=getattr(settings, 'SITE_ID', 1), defaults={'domain': 'app.localhost', 'name': 'App Localhost'})
+site, _ = Site.objects.get_or_create(id=getattr(settings, 'SITE_ID', 1), defaults={'domain': 'app.localhost', 'name': 'App Localhost'})
+app, _ = SocialApp.objects.get_or_create(provider='google', defaults={'name': 'Google Auth', 'client_id': 'DUMMY', 'secret': 'DUMMY'})
+app.sites.add(site)
 "
 
 echo "Starting gunicorn..."
