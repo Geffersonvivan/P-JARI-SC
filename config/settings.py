a78ef3a739f01bdbcd5fa1a39aa33b2560ed56d2
Gefferson Vivan <geffersonvivan@192.168.19.246>
2026-02-27 10:51:10 -0300

    feat: enforce email verification and set up Gmail SMTP

--- config/settings.py
+++ config/settings.py
@@ -102,10 +102,20 @@ SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
-# Envio de e-mail para o terminal (Simulação de envio p/ Dev Local)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if DEBUG:
+    # Envio de e-mail para o terminal (Simulação de envio p/ Dev Local)
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    # Envio real via Gmail (Produção)
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'pjarisc@gmail.com')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'gvgv@2020X')
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Pular a tela intermediária chata do allauth "sign in via..."
