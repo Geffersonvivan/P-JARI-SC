fc6bdf2767dc2b1b579de42ed2fafe5989221b3e
Gefferson Vivan <geffersonvivan@MacBook-Air-de-Gefferson.local>
2026-02-28 21:20:39 -0300

    fix(deploy): configure dj_database_url to prioritize DATABASE_URL in production

--- config/settings.py
+++ config/settings.py
@@ -159,8 +159,10 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
+        env='DATABASE_URL',
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
